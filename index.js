3a04c43164d2df2f9028f474b4dec0f5966d44a2
Mark Baker <mrbaker1917@gmail.com>
2018-07-18 21:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object.key = value;
   return object;
 }
-console.log(updateObjectWithKeyAndValue(recipes, prop1, 1));
+console.log('updateObjectWithKeyAndValue(recipes, prop1, 1)');
